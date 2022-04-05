096ea50b432d782ec887992c7a97d3346f0f8154
Rafael Cunha <contatorafaeloliveiradacunha@gmail.com>
2022-04-05 20:35:52 -0300

    fix(gif): url updated

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ const App = () => {
       
       {movieList.length <= 0 &&
         <div className='loading'>
-          <img src='https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif' alt='Carregando...'/>
+          <img src='https://www.rchandru.com/images/portfolio/loading.gif' alt='Carregando...'/>
         </div>
       }
       
